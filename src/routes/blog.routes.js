530537e29c8e652f8b6bb69dd1e1fcf4fe049374
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2025-07-17 12:43:31 +0100

    Set up server and database configuration

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -3,6 +3,6 @@ const blogRoutes = express.Router()
 
 // Get all blogposts
 
-// Search for a particular blogpost by params
+// Search for a particular blogpost by params 
 
 export default blogRoutes;
